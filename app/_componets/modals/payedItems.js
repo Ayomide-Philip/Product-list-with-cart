106f4d8e2a6d7f91b313d1cc163e69983c75f204
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-10-16 23:19:33 +0100

    updating my odebase

--- app/_componets/modals/payedItems.js
+++ app/_componets/modals/payedItems.js
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-export default function PaidItem() {
+export default function PaidItem({ quantity, price, name }) {
   return (
     <div className="flex items-center gap-3 justify-between">
       <div className="flex gap-3">
@@ -11,15 +11,17 @@ export default function PaidItem() {
           />
         </div>
         <div className="flex flex-col justify-center">
-          <h1 className="font-bold text-[14px]">Classic Tiramisu</h1>
+          <h1 className="font-bold text-[14px]">{name}</h1>
           <p className="font-[300] text-[12px]">
-            <span className="text-[hsl(14,86%,42%)] font-bold mr-2">1x</span>
-            @$4.00
+            <span className="text-[hsl(14,86%,42%)] font-bold mr-2">
+              {quantity}x
+            </span>
+            @${Number(price).toFixed(2)}
           </p>
         </div>
       </div>
       <div className="flex">
-        <p className="font-normal">$5.50</p>
+        <p className="font-normal">${(quantity * price).toFixed(2)}</p>
       </div>
     </div>
   );
