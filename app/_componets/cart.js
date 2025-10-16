15cfac71aa32a4619261101cd273bb96ac7ef0d4
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-10-16 21:19:57 +0100

    updating my cart code

--- app/_componets/cart.js
+++ app/_componets/cart.js
@@ -1,13 +1,16 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import GridCard from "./cardGrid";
 import Order from "./order";
-
+import data from "./data.json";
 export default function Cart() {
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState(data);
+  useEffect(() => {
+    console.log(cart);
+  }, [cart]);
   return (
     <div className="flex flex-col md:flex-row w-full gap-5">
-      <GridCard setCart={setCart} />
+      <GridCard setCart={setCart} data={data} />
       <Order cart={cart} />
     </div>
   );
