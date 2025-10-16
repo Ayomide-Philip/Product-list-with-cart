38af4202c426f85c57ff467412adde03b0b6454c
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-10-16 19:00:30 +0100

    Creating each button as a componet and importing it

---
+++ app/_componets/button/addToCart.js
@@ -0,0 +1,13 @@
+/* eslint-disable @next/next/no-img-element */
+export default function AddToCart() {
+  return (
+    <button className="bg-white text-black p-2 px-5 border-black border-1 rounded-2xl flex text-sm hover:text-[hsl(14,86%,42%)] cursor-pointer hover:border-[hsl(14,86%,42%)]">
+      <img
+        src="/images/icon-add-to-cart.svg"
+        alt="Add to cart"
+        className="text-[hsl(14,86%,42%)]"
+      />
+      Add to Cart
+    </button>
+  );
+}
--- app/_componets/card.js
+++ app/_componets/card.js
@@ -1,3 +1,5 @@
+import AddToCart from "./button/addToCart";
+
 /* eslint-disable @next/next/no-img-element */
 export default function Card({ name, category, price, desktop }) {
   return (
@@ -9,13 +11,11 @@ export default function Card({ name, category, price, desktop }) {
           className="w-full md:w-[250px] h-[200px] rounded-2xl object-cover hover:border-[hsl(14,86%,42%)] hover:border-2 cursor-grab"
         />
 
-        <div className="absolute  left-1/2 transform -translate-1/2">
-          <button className="bg-white text-black p-2 px-5 rounded-2xl">
-            Add to Cart
-          </button>
+        <div className="absolute left-1/2 transform -translate-1/2 w-full flex justify-center items-center">
+          <AddToCart />
         </div>
       </div>
-      <div className="mt-3">
+      <div className="mt-5">
         <span className="font-light text-sm">{category}</span>
         <p className="font-bold">{name}</p>
         <p className="text-[hsl(14,86%,42%)] font-bold">${price.toFixed(2)}</p>
