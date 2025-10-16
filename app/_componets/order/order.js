fe6b44748df349a53ffdabe70164b128bffdaff4
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-10-16 22:19:06 +0100

    updating my order code

--- app/_componets/order/order.js
+++ app/_componets/order/order.js
@@ -1,9 +1,22 @@
 /* eslint-disable @next/next/no-img-element */
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CartItem from "../cartItem";
 
 export default function UserOrder({ cart }) {
   const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    setTotal(generateTotal(cart));
+  }, [cart]);
+
+  function generateTotal(cart) {
+    let sum = 0;
+    cart.map(({ price, quantity }) => {
+      sum = sum + price * quantity;
+    });
+    return sum;
+  }
+
   return (
     <>
       <div className="flex flex-col">
@@ -15,7 +28,9 @@ export default function UserOrder({ cart }) {
       </div>
       <div className="flex justify-between mt-3 items-center">
         <p className="font-light">Order Total</p>
-        <p className="font-semibold text-2xl red-hat-text">$46.50</p>
+        <p className="font-semibold text-2xl red-hat-text">
+          ${total.toFixed(2)}
+        </p>
       </div>
       <div className="flex justify-center items-center bg-[hsl(13,31%,94%)] p-3 gap-2 mt-3 rounded-xl">
         <img
