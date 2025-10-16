e2c6f9be6eb13e32e52a75c0cf516d36533bc008
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-10-16 21:14:24 +0100

    updating my button header by removing un used props

--- app/_componets/buttonHeader.js
+++ app/_componets/buttonHeader.js
@@ -2,12 +2,11 @@
 import { useState } from "react";
 import AddToCart from "./button/addToCart";
 import Counter from "./button/counter";
-export default function ButtonHeader() {
-  const [count, setCount] = useState(0);
+export default function ButtonHeader({ count, setCount, setCart, name }) {
   return (
     <div className="absolute left-1/2 transform -translate-1/2 w-full flex justify-center items-center">
       {count === 0 ? (
-        <AddToCart setCount={setCount} />
+        <AddToCart setCount={setCount} setCart={setCart} name={name} />
       ) : (
         <Counter setCount={setCount} count={count} />
       )}
